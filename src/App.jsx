667a7103cd489dc3e6a72e9b9282cfd2e86d0369
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-11 12:46:36 -0500

    moder js Organization style

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,59 @@
+function Header() {
+  return(<header>
+    <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+  </header>);
+}
+
+function PostNew() {
+  return(<div id="posts-new">
+  <h1>New post</h1>
+  <form>
+  <div>
+    Title: <input type="text" />
+  </div>
+  <div>
+    content: <input type="text" />
+  </div>
+  <div>
+  <button type="submit">Create post</button>
+  </div>
+  </form>
+</div>);
+}
+
+function PostIndex() {
+  return(<div id="posts-index">
+  <h1>All posts</h1>
+  <div className="posts"/>
+    <h2>Mud Pie</h2>
+    <p>content: i made a mud pie today</p>
+    <p>author: Jay Wengrow</p>
+    <button>More info</button>
+</div>);
+}
+
+function Footer() {
+  return(<footer>
+    <p>Copyright 20XX</p>
+  </footer>);
+}
+
+
 function App() {
   return (
     <div>
-      <header>
-        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
-      </header>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
 
-      <div id="posts-new">
-        <h1>New post</h1>
-        <form>
-        <div>
-          Title: <input type="text" />
-        </div>
-        <div>
-          content: <input type="text" />
-        </div>
-        <div>
-        <button type="submit">Create post</button>
-        </div>
-
-        </form>
-      </div>
-
-      <div id="posts-index">
-        <h1>All posts</h1>
-        <div className="posts"/>
-          <h2>Mud Pie</h2>
-          <p>content: i made a mud pie today</p>
-          <p>author: Jay Wengrow</p>
-          <button>More info</button>
-      </div>
-
-      <footer>
-        <p>Copyright 20XX</p>
-      </footer>
+function Content() {
+  return (
+    <div>
+      <PostNew />
+      <PostIndex />
     </div>
   );
 }
